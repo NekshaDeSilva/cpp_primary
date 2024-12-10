6648ea4fc451c9b0a96fbeb32784c1c142da849f
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2024-12-10 13:40:00 +0530

    Pointers with functions study

---
+++ pointers/pointers-in-functions2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <thread>
+using namespace std;
+
+int main(){
+    //new int - this allocates memory in the heap
+}
+
+//POINT: 
+
+//memory in cpp, divided into two (2) areas.
+// 1. Heap memory
+// 2. Stack memory
+
+//refer the readme for m,ore info
+
+int main(){
+    int somevalue; // This is a variable went to the stacxk memory. /temp
+    new int somevalued; // This is a variable went to the heap memeory /permanent unless deleted.
+
+
+
+
+}
+
+
