55136f19fbc7d6198c6fc0abb3cdc2031b91e25d
Neksha DeSilva <nekshavs@gmail.com>
2025-05-17 18:30:31 +0530

    updated

--- edu/relationalOps.cpp
+++ edu/relationalOps.cpp
@@ -3,3 +3,11 @@
 #include <string>
 
 using namespace std;
+int main(){
+    int lower = 100;
+    int higher = 100;
+    cout << "The 100 to 300 range is: " << (lower == higher) << endl;
+    //it output [1] if the relational operators call is true. 
+    //Here, the 100 is similar to 100, so the output should be defined as "1"
+    
+}
