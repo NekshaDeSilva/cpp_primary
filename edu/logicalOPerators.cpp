c5b29fa641e339398edb500cb6f8e1804a1b354a
Neksha DeSilva <nekshavs@gmail.com>
2025-05-21 12:34:04 +0530

    updated

--- edu/logicalOPerators.cpp
+++ edu/logicalOPerators.cpp
@@ -3,3 +3,18 @@
 #include <string>
 using namespace std;
 
+//Biwise operators usage is displayed here.
+
+int main(){
+  int bitwiseAND= 1&4;
+  int bitwiseOR = 2|6;
+  int bitwiseXOR= 9^5;
+  cout << bitwiseXOR << endl;
+  cout << bitwiseOR << endl;
+cout << bitwiseAND << endl;
+unsigned long long int number =150495045045845;
+cout << "The number is: " << number << endl;
+
+
+    
+}
