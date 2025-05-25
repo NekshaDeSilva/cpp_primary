0f1490039a6c229524666044bcc01bb008cfac31
Neksha DeSilva <nekshavs@gmail.com>
2025-05-25 17:37:09 +0530

    feat: add interactive test functionality and evaluation feedback for operator precedence

--- edu/operatorpresedence.cpp
+++ edu/operatorpresedence.cpp
@@ -14,7 +14,18 @@ int runtheSequence(const string& exp) {
     cout << "Result: " << result << endl;
     return result;
 }
+int correctIncorrectMeasure(const string& state){
+    cout << "Evaluating: your answer.." << endl;
+    if(state == "correct"){
+        cout<< "Correct" << endl;
 
+
+    }else{
+        cout <<  "Incorrect, Try Again!" << endl;
+    }
+    return 0;
+    
+}
 int main() {
     string expe = "8*(8/4)/2+4";
     cout << "Welcome to Operator Precedence in C++!\n";
@@ -30,7 +41,45 @@ int main() {
     runtheSequence(expe);
     cout << "Would you like to take a test of Operator Presedence in C++ for XOR, OR, NOT and other operators too?" << endl;
     this_thread::sleep_for(chrono::seconds(2));
+    expe = "8^3(2)+4-2*4";
+    cout << "What is the Answer for this: " << expe << endl;
+     for (int i = 10; i > 0; i--) {
+        cout << i << " second(s) left" << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    runtheSequence(expe);
+    cout << "Ahh That was a bit hard; So now try this:" << endl;
+    expe = "~4&2*2+10" ;
+
+    cout << "What is the Answer for this: " << expe << endl;
+     for (int i = 10; i > 0; i--) {
+        cout << i << " second(s) left" << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    runtheSequence(expe);
+    cout << "Okay, Now the test mode!" << endl;
+    cout << "\n\n" ; 
+    int the_valueReturned = runtheSequence(expe);
+    cout << "🔴 From now on, type your answer in the space given, while we are counting."<< endl;
+    expe= "~1*100(~1)^6^5";
+    cout << "What is the answer for this: " << expe << endl;
+    int the_valueEntered;
+    cin >> the_valueEntered;
+    for (int i = 10; i > 0; i--) {
+        cout << i << " second(s) left" << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+   
+
     
+    if(the_valueEntered == the_valueReturned){
+        correctIncorrectMeasure("correct");
+
+    }else{
+        correctIncorrectMeasure("incorrect");
+    }
+
+
 
     return 0;
 }
