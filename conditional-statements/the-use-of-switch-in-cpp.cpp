8eae4d60d08fc4ebd62f23b3e386eb50df91f32b
NekshaDeSilva <rohandesilvahotmail@gmail.com>
2025-05-31 11:33:10 +0530

    updated

---
+++ conditional-statements/the-use-of-switch-in-cpp.cpp
@@ -0,0 +1,11 @@
+//This is an example of switch statement in conditions.
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+int main(){
+    //write your code here man
+}
