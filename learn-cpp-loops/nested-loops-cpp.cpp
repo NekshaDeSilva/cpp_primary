abc604d48b303ca207e336c0778ed8a330cd969c
NekshaDeSilva <rohandesilvahotmail@gmail.com>
2025-06-06 12:41:36 +0530

    updated

---
+++ learn-cpp-loops/nested-loops-cpp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+int main(){
+    cout << "Welcome to Nested loops. I will teach you through Nested loops as we see in this tutorial!" << endl;
+    //You need to create a multiplication table of 6,7,8,9 numbers and from 1x to 10x per each. How can you do this? 
+
+    //example code 
+
+    for (int theMainNumber =6; theMainNumber < 10; theMainNumber++){
+      cout << "The Table of " << theMainNumber << " is" << endl;
+      for (int innernumber = 1; innernumber< 11; innernumber++){
+        cout << theMainNumber << " x " << innernumber << " = " << (theMainNumber * innernumber) << endl;
+
+      }
+      cout << "\n ------------------------------------------------------------ \n" << endl;
+      this_thread::sleep_for(chrono::seconds(2));
+
+    }
+}
