20dbd33884997921753c4b37a02b8b5d56ace73e
NekshaDeSilva <rohandesilvahotmail@gmail.com>
2025-06-12 22:16:24 +0530

    updated

--- learn-basics/challenge-get-the-output-when-the-base-and-exponent-are-given/challenge-get-the-output-when-the-base-and-exponent-are-given.cpp
+++ learn-basics/challenge-get-the-output-when-the-base-and-exponent-are-given/challenge-get-the-output-when-the-base-and-exponent-are-given.cpp
@@ -7,5 +7,7 @@
 
 using namespace std;
 int main(){
+    //Today I'm going to learn up the challenge that was a headache to me in the past.
+    
     //the exponent and the base are given, so we need to
 }
