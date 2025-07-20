24292a45848c101f36736fc8bf53e0776cc31197
NekshaDeSilva <rohandesilvahotmail@gmail.com>
2025-07-20 12:22:37 +0530

    updated

---
+++ cpp-recursion/recusion-basics-printing-numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int printingNumbers(int status =1){ //In there the status means that when status is 1, is means we print numbers from n to N and when the status is 0, it means that we have to print those letters N downto n.
+
+
+}
+ int main(){
+
+}
+
+
+// DEMO: PASS BY VALUE:  IN PASS BY VALUES, WE ALSWAYS ADD THE PARAMEETR AS IN  THE NORMAL WAY: 
+
+// PASS  by reference: we always add a & after the data type bacause it refers to the original value inhte main fucntion.
+
+// So, we  can add the pass by   REFRENJCE AS FOLLOWS ;
+
+/*   
+        int somevalu||_ewithsoefucntion90(int& somevalue){
+     
+        }
+
+        int main(){
+     long long int somevalue = 99383;
+
+        }     
+*/
