411d479b46ef94c69363de50ba0f1ecaf89296cc
NekshaDeSilva <rohandesilvahotmail@gmail.com>
2025-07-24 20:07:16 +0530

    updated

--- arrays/2Dimensional/average-analyzer-of-2D-data.cpp
+++ arrays/2Dimensional/average-analyzer-of-2D-data.cpp
@@ -11,7 +11,7 @@ int main(){
             6,6,5,1,1
         },
         {
-            7,11,5,1,1
+            6,99,5,21,1
         },
         {
             6,99,5,21,1
@@ -19,5 +19,40 @@ int main(){
         {
             6,6,5,1,1
         }
+    };
+    int rowavg[R];
+    for(int i = 0; i < R; i++){
+        int tempAVg=0;
+        for (int x = 0;x < C; x++  ){
+            cout << arrayXD[i][x] << " " ;
+            tempAVg += arrayXD[i][x];
+        }
+        rowavg[i] = tempAVg/C;
+        cout << endl;
     }
+    cout << endl;
+    cout << "Row averages: ";
+    int high = 0;
+    int samehighs = 0;
+    for (int i = 0; i < R; i++) {
+        cout << rowavg[i] << " ";
+        if(rowavg[i]>= high){
+            high = rowavg[i];
+            
+            }
+        }
+        for (int i =0; i < C; i++){
+                if(rowavg[i] == high){
+                    samehighs++;
+                    
+                }
+    }
+    
+
+
+    cout <<  "Highest Average: " << high <<  "%" << endl;
+    if(samehighs > 1 ){
+        cout << "Same high avg count " << samehighs << endl;
+    }
+    cout << endl;
 }
