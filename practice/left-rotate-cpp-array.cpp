189b69163e7d7e351caba92217ff1855b77142e7
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-05 06:31:28 +0530

    updated

---
+++ practice/left-rotate-cpp-array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <thread>
+using namespace std;
+
+void left_rotate(int array[], int size){
+    int anotherarray[size] ={};
+    int numeration =0 ;
+    for (int i =8; i >-1; i--){
+        numeration++;
+        anotherarray[numeration] = array[i];
+
+    }
+    for (int i =0; i < 10; i++){
+        array[i] = anotherarray[i];
+    }
+}
+int main(){
+    int existingarraypp[10] = {1,2,3,4,5,6,7,8,9,10};
+    left_rotate(existingarraypp, 10);
+    for (int i =0; i < 10; i++){
+        cout << existingarraypp[i] << endl;
+    }
+}
