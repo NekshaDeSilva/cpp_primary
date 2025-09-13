d4859823b5023cfcacf0c06d5afec39a9cc016b5
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-13 12:38:23 +0530

    updated

--- pointers/readme.md
+++ pointers/readme.md
@@ -15,3 +15,16 @@ Cpp pointers are a some sort of variable that remebers the memory adddresses of 
 A pointer is an address to a a variabe and it shows not the value of the varaible but the address where it as saved or located in.
 
 
+Prerequisities : 
+# In programming, especialyu in C++,  you must know that the SIZE is not equal to the word LENGTH is any tems. 
+## Hence, BeCAUSE, IN PROGRAMMING,
+```c++
+int main(){
+    int somevalue = 998;
+    int *pointer = &somevalue; // Added the * mark because it was thje first-declare of that pointer.
+    
+
+}
+```
+### In the above code snippet. the length of the ```somevalue``` is 9. But the size of the integer can be far more big.
+### In the above example, the length of the pointer ```pointer``` can depend on the size of the pointer address and it is not an absolute or a fixed value. BUT, the SIZE of the pointer is always 8 Bit on 64-bit operating systems and 4-bits on 32 bit operating systems.
--- pointers/showing-the-address-of-a-variable-using-pointers.cpp
+++ pointers/showing-the-address-of-a-variable-using-pointers.cpp
@@ -21,3 +21,34 @@ int main(){
     cout << *location << endl;
 
 }
+
+
+//We can modify the pointer address but the modification of the variable should be the same as the prvious one, that means it should be the same type as before.
+
+int main(){
+    //the first variable and the pointer addresssing its memory.
+    int something = 990;
+    int *ptr = &something; //Adding the * mark when first declaring the pointer. 
+
+    //changing the address of the opinter to ANOTHER VARIABLE
+    int another_thing = 7754;
+    ptr = &another_thing // We never add the * mark again when we just need to to change te address of the pointer.
+
+}
+
+//THE WILD Pointer
+
+// a wild pointer is a pointer where its value is irrelevent and random. When we create a pointer without any declarsion of a value, then, the pointer becomes a wild pointer where its value is nothing good.
+
+int main(){
+    int *wild;
+    cout << wild;
+ // Prints a invalid address where no value exists there.
+
+}
+//THE NULL POINTER.
+int main(){
+    int  *nullpointer = NULL;
+    //tHE NULL POINTER IS USED TO CREATE A NULL address value when it's necesssory to create a pointer but not needed to pass a value into it.
+    
+}
