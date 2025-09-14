e04a4d54e0821e1aff700de1e5bb5ecd277d0d94
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-14 16:33:22 +0530

    updated

--- codeforces/1050/A.cpp
+++ codeforces/1050/A.cpp
@@ -1,50 +1,9 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 #include <string>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <deque>
+#include <thread>
 
-// For fast I/O
-void setup_io() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-}
-
-// Data types and aliases
-using ll = long long;
-using vi = std::vector<int>;
-using vll = std::vector<ll>;
-using pii = std::pair<int, int>;
-using pll = std::pair<ll, ll>;
-
-// Macros for loops
-#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define REP(i, n) FOR(i, 0, n)
-#define ALL(x) (x).begin(), (x).end()
-#define sz(x) (int)(x).size()
-
-// Main problem-solving logic function
-void solve() {
-    // Write your solution here
-    int n;
-    std::cin >> n;
-    // ...
-}
-
-int main() {
-    setup_io();
-    
-    int t = 1; // Default to 1 test case
-    // std::cin >> t; // Uncomment this line if there are multiple test cases
-
-    while (t--) {
-        solve();
-    }
-    
-    return 0;
+using namespace std;
+int main(){
+    int 
 }
---
+++ cpp-recursion/readme.md
@@ -0,0 +1,8 @@
+# YOU MUST  BE ABLE TO SOLVE THESE THINGS AND THIS IS A MUST.
+By the end of tomorrow, you must be able to solve these probles in this website. 
+
+https://www.w3resource.com/cpp-exercises/recursion/index.php
+
+and upload all of the code YOU WROTE into the folder called cpp-recusion and it should and must be really working and perfectly fine. AND also, you MUST LEARN **The special technique called "Backtracking as well."**
+
+[image](https://www.w3resource.com/w3r_images/cpp-recursion-exercise-flowchart-12.png)
