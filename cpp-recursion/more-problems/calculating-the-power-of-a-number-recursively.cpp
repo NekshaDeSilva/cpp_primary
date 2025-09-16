8f4df643dea5fd8165d71c27acc4bd1d52b8f161
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-16 21:52:18 +0530

    updated

--- cpp-recursion/more-problems/calculating-the-power-of-a-number-recursively.cpp
+++ cpp-recursion/more-problems/calculating-the-power-of-a-number-recursively.cpp
@@ -5,10 +5,27 @@
 
 using namespace std;
 
-int power(){}
+int power(int number, int exponent){
+    if(exponent == 0){
+        return 1;
+    }else{
+      
+
+        return number * power(number, exponent -1);//IF WE ADD ***exponent--*** here, it will lead to a segmentaion fault because it will lead to a error. The reason is when we use ***exponent--*** it doesnt actiually update the value of the exponent.
+
+    }
+}
 
 int main(){
     cout << "Power of the number generator" << endl;
     cout << "\n Please enter your number to see the power" << endl;
-    
+    string number;
+    getline(cin, number);
+    int numberconv = stoi(number);
+    cout << "Please the number of times you want to exponent the number." << endl;
+    string exponent; 
+    getline(cin, exponent);
+    int exponentconv = stoi(exponent);
+    cout << number << " To the power " << exponent << " is,  " << power(numberconv, exponentconv) << endl;
+
 }
