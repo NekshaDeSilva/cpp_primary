abcd823abcaa2f9eed6a26883336d736530bb407
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-16 21:26:15 +0530

    updated

--- cpp-recursion/more-problems/calculating-the-factorial-of-a-number.cpp
+++ cpp-recursion/more-problems/calculating-the-factorial-of-a-number.cpp
@@ -1,13 +1,23 @@
-#include <iostrean>
+#include <iostream>
 #include <iomanip>
 #include <string>
 #include <thread>
 #include <vector>
 
 using namespace std;
-int recurse(){
-    
+int recurse( int number){
+    if(number  ==0 ){
+        return 1;
+
+    }else{
+        
+        
+       
+        return number * recurse(number-1);
+
+    }
 }
 int main(){
-
+    long long int number = 10;
+    cout << recurse(number);
 }
---
+++ cpp-recursion/more-problems/calculating-the-power-of-a-number-recursively.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+int power(){}
+
+int main(){
+    cout << "Power of the number generator" << endl;
+    cout << "\n Please enter your number to see the power" << endl;
+    
+}
