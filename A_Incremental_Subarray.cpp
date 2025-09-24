cb35b55942780d8c387afd12bda917f09bbd29f5
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-09-24 20:16:22 +0530

    updated

---
+++ A_Incremental_Subarray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+     
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> t;        
+    
+    while(t--) {     
+
+        
+        
+    }
+    
+    return 0;
+}
+
+//THINGS I LEARNT
+
+//a subarray is a unbroken sequence of numbers in an array.
+// ex: int arry[4] = {1,2,3,4}
+// the all posible sub arrays:
+//  1
+//  2
+//  3
+//  4
+// 1,2
+// 2,3
+/// 3,4
+// 1,2,3
+// 2,3,4
+// 1,2,3,4 
---
+++ practice/multiplying_two_matrices.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <thread>
+using namespace std;
+void multiplication(int arr1[][2],int row1, int col1,  int arr2[][2], int row2, int col2, int result[][2]){
+    
+}
+int main(){
+    multiplication({{1,2} , {5,7}} , 2 , 2 , {{10,20} , {-30,70}} , 2 , 2 , {{0,0} , {0,0}});
+
+}
