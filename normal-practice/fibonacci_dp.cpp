8dfcfbd0353e68efe18ed3b837e224cfc7ce86e5
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-10-02 12:22:00 +0530

    updated

---
+++ normal-practice/fibonacci_dp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+int n;
+cin >> n;
+
+vector<int> dp(n+1);
+dp[0] = 0;
+dp[1] = 1;
+
+for(int i=2;i<=n;i++){
+dp[i] = dp[i-1] + dp[i-2];
+}
+
+cout << dp[n] << endl;
+
+
+return 0;
+}
