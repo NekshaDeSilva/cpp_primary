001fbc57631254f9985e1df5c40c5322ee8084fa
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-10-02 14:05:00 +0530

    updated

---
+++ codeforces-archive/knapsack_01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+int n,W;
+cin >> n >> W;
+
+vector<int> w(n+1), v(n+1);
+for(int i=1;i<=n;i++){
+    cin >> w[i] >> v[i];
+}
+
+vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+
+for(int i=1;i<=n;i++){
+    for(int j=0;j<=W;j++){
+        dp[i][j] = dp[i-1][j];
+        if(j >= w[i]){
+            dp[i][j] = max(dp[i][j], dp[i-1][j-w[i]] + v[i]);
+        }
+    }
+}
+
+
+cout << dp[n][W] << endl;
+
+return 0;
+}
