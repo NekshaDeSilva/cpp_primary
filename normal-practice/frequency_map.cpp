ec4ae1a44606dda97335360cb7fadf24e1b99aa1
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2025-10-03 10:15:00 +0530

    updated

---
+++ normal-practice/frequency_map.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+int n;
+cin >> n;
+
+map<int,int> freq;
+
+for(int i=0;i<n;i++){
+    int x;
+    cin >> x;
+    freq[x]++;
+}
+
+
+for(auto p : freq){
+    cout << p.first << " " << p.second << "\n";
+}
+
+return 0;
+}
