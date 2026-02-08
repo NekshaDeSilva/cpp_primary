b53e3314a29c99fef275efedead397048aa16c38
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-02-08 09:24:02 +0530

    UPDATED

--- functions/fucntion-overloading/challenge_2.cpp
+++ functions/fucntion-overloading/challenge_2.cpp
@@ -3,7 +3,15 @@
 #include <cmath>
 
 using namespace std;
+int calctherating_people(int worogbutmade, int made){
+    int result  = worogbutmade + made;
 
+    return result;
+}
 int main(){
+        int N,A,B;
+        cin >> N >> A >> B;
         
+    cout << calctherating_people(B, N-(A+B)) <<  " "  << N-(A+B)<< endl;
+    
 }
